8d6b7d2c8dd0c6005ce49bdc11a49eeee8774e6e
Harsh-Jaju <cs24b076@smail.iitm.ac.in>
2025-09-20 15:29:08 +0530

    Add files via upload

---
+++ Project 5/Fill.asm
@@ -0,0 +1,53 @@
+// This file is part of www.nand2tetris.org
+// and the book "The Elements of Computing Systems"
+// by Nisan and Schocken, MIT Press.
+// File name: projects/4/Fill.asm
+
+// Runs an infinite loop that listens to the keyboard input. 
+// When a key is pressed (any key), the program blackens the screen,
+// i.e. writes "black" in every pixel. When no key is pressed, 
+// the screen should be cleared.
+
+//// Replace this comment with your code.
+// This loop colors the 32 blocks of the first row black.
+(LOOP)
+    @KBD 
+    D=M 
+    @i
+    M=0
+    @BLACK 
+    D;JNE
+(WHITE)
+    @i
+    D=M
+    @8192
+    D=D-A
+    @LOOP
+    D;JGE
+
+    @SCREEN
+    D=A     
+    @i
+    A=D+M   
+    M=0   
+    @i
+    M=M+1
+    @WHITE
+    0;JMP
+(BLACK)
+    @i
+    D=M
+    @8192
+    D=D-A
+    @LOOP
+    D;JGE
+
+    @SCREEN
+    D=A     
+    @i
+    A=D+M   
+    M=-1   
+    @i
+    M=M+1
+    @BLACK
+    0;JMP
---
+++ Project 5/Mult.asm
@@ -0,0 +1,36 @@
+// This file is part of www.nand2tetris.org
+// and the book "The Elements of Computing Systems"
+// by Nisan and Schocken, MIT Press.
+// File name: projects/4/Mult.asm
+
+// Multiplies R0 and R1 and stores the result in R2.
+// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
+// The algorithm is based on repetitive addition.
+
+@count
+M=1
+@R1
+D=M
+@n
+M=D
+@R2
+M=0
+(LOOP)
+    @count
+    D=M
+    @n
+    D=D-M
+    @STOP
+    D;JGT
+
+    @R0
+    D=M
+    @R2
+    M=M+D
+    @count
+    M=M+1
+    @LOOP
+    0;JMP
+(STOP)
+    @STOP
+    0;JMP
